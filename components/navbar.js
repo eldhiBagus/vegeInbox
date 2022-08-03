06f5af4cfc871fa77a40c44e7d0f70343cc1f1d5
eldhiBagus <baguseldhi@gmail.com>
2022-08-03 13:35:07 +0700

    add  passhref

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/link-passhref */
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,7 +8,7 @@ function Navigasi() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand><Link href="/">
+        <Navbar.Brand><Link href="/" passHref>
             <Image
               alt="hehe"
               src="/caca.png"
@@ -20,9 +19,9 @@ function Navigasi() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="ms-5"><Link href="/"><b>Home</b></Link></Nav.Link>
-            <Nav.Link className="ms-5"><Link href="/abouts"><b>About Us</b></Link></Nav.Link>
-            <Nav.Link className="ms-5"><Link href="/product"><b>Products</b></Link></Nav.Link>
+            <Nav.Link className="ms-5"><Link href="/" passHref><b>Home</b></Link></Nav.Link>
+            <Nav.Link className="ms-5"><Link href="/abouts" passHref><b>About Us</b></Link></Nav.Link>
+            <Nav.Link className="ms-5"><Link href="/product" passHref><b>Products</b></Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
